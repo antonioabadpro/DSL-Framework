bb8032861a28c13a32d902d0ba1043619c600def
agustinrodriguez <agustinrodriguez@localhost>
2025-10-29 09:12:39 +0100

    Clases prueba.

---
+++ src/Conectores/Conector.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conectores;
+
+/**
+ *
+ * @author agustinrodriguez
+ */
+public class Conector {
+    
+}
